72500d5d784c0247bcdb4fec2ad63a6c1b2de132
emsyzz <emsynet@gmail.com>
2014-02-16 11:11:47 +0200

    Added setter method to dataObj

--- class/data.php
+++ class/data.php
@@ -8,6 +8,10 @@ class dataObj extends stdClass {
 		$this->__stack = $data;
 	}
 	
+	public function __set($name, $value) {
+		$this->__stack[$name] = $value;
+	}
+	
 	public function __get($name) {
 		if(isset($this->__stack[$name])) {
 			return $this->__stack[$name];
